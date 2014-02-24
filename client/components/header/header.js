51b45f7d2d4bf784ddb508c63bc233c589ea7a29
Adam Brodzinski <adambrodzinski@gmail.com>
2014-02-23 20:00:35 -0800

    re-adjust the margins so that it's always centered
    Keeping the text centered when it's not overflowing is difficult while
    keeping
    it compat. with older androids. This will manually adjust the margins
    so that
    different button widths can be used and still maintain center.

--- client/components/header/header.js
+++ client/components/header/header.js
@@ -7,5 +7,23 @@ Template.header.events({
 });
 
 Template.header.rendered = function() {
-  
+  adjustCenterHeaderMargins();
 };
+
+
+/** Adjusts the margins of the center text container on
+ *  the header. This allows the text to overflow and still
+ *  be centered with buttons of different widths.
+ * @api private
+ */
+function adjustCenterHeaderMargins() {
+  var buttons = $('.hdr-btn'),
+      leftW = buttons.first().width(),
+      rightW = buttons.last().width();
+
+  $('.hdr-middle').css({
+    'margin-left': '-' + leftW + 'px',
+    'margin-right':'-' + rightW + 'px'
+  });
+}
+
